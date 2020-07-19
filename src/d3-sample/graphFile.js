dd530b5e88c920443b62bef9ac514578b85708b4
Mesut Yılmaz <im.mesut.yilmaz@gmail.com>
2020-07-19 12:41:01 +0300

    visualization - network graph basic structure completed

---
+++ src/d3-sample/graphFile.js
@@ -0,0 +1,49 @@
+data = {
+    "nodes": [{
+            "name": "node1",
+            "id": 1,
+            "size": 10
+        },
+        {
+            "name": "node2",
+            "id": 2,
+            "size": 30
+        },
+        {
+            "name": "node3",
+            "id": 3,
+            "size": 40
+        },
+        {
+            "name": "node4",
+            "id": 4,
+            "size": 30
+        }
+    ],
+    "links": [{
+            "source": 2,
+            "target": 1,
+            "weight": 2
+        },
+        {
+            "source": 3,
+            "target": 4,
+            "weight": 3
+        },
+        {
+            "source": 4,
+            "target": 1,
+            "weight": 5
+        },
+        {
+            "source": 2,
+            "target": 4,
+            "weight": 3
+        },
+        {
+            "source": 3,
+            "target": 1,
+            "weight": 2
+        }
+    ]
+}
---
+++ src/d3-sample/visualization.js
@@ -0,0 +1,79 @@
+const width = 600;
+const height = 600;
+
+const font_base = 10;
+const font_scale = 2;
+const spacing = 5;
+
+var svg = d3.select("#my_dataviz").append("svg")
+  .attr("width", width)
+  .attr("height", height)
+  .append("g");
+
+// d3.json("/graphFile.json", function (data) { });
+
+// data is from graphFile.js
+
+var link = svg.selectAll("line").data(data.links).enter().append("line")
+  .attr("stroke-width", (d) => {
+    return d.weight
+  })
+  .style("stroke", "#aaa")
+
+var node = svg.selectAll("circle").data(data.nodes).enter().append("circle")
+  .attr("r", (d) => {
+    return d.size
+  })
+  .style("fill", "#69b3a2")
+
+var label = svg.append("g").attr("class", "labels").selectAll("text").data(data.nodes).enter().append("text")
+  .attr("class", "label")
+  .style("font-size", (d) => {
+    return font_base + font_scale * (d.size / font_base)
+  })
+  .text((d) => {
+    return d.name
+  })
+
+var simulation = d3.forceSimulation(data.nodes)
+  .force("link", d3.forceLink()
+    .id((d) => {
+      return d.id
+    })
+    .links(data.links)
+  )
+  .force("charge", d3.forceManyBody().strength(-10000))
+  .force("center", d3.forceCenter(width / 2, height / 2))
+  .on("end", ticked);
+
+function ticked() {
+  link
+    .attr("x1", function (d) {
+      return d.source.x;
+    })
+    .attr("y1", function (d) {
+      return d.source.y;
+    })
+    .attr("x2", function (d) {
+      return d.target.x;
+    })
+    .attr("y2", function (d) {
+      return d.target.y;
+    });
+
+  node
+    .attr("cx", function (d) {
+      return d.x;
+    })
+    .attr("cy", function (d) {
+      return d.y;
+    });
+
+  label
+    .attr("x", function (d) {
+      return d.x + d.size + spacing
+    })
+    .attr("y", function (d) {
+      return d.y + (d.size / font_base) / 2 + font_base / 2
+    })
+}
